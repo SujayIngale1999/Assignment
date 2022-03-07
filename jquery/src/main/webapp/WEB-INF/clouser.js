ca4d30d93b7ab4d715c61ed03ee63304dbd4d9ae
SujayIngale1999 <98298872+SujayIngale1999@users.noreply.github.com>
2022-03-07 14:24:21 +0530

    done
    commit

---
+++ jquery/src/main/webapp/WEB-INF/clouser.js
@@ -0,0 +1,21 @@
+function myFunction(){
+    var name="sujay Inagle";
+    function printName(){
+        alert("My Name is : "+name);
+        console.log("My Name is : "+name);
+    }
+    printName();
+}
+myFunction();
+var yourFunction=myFunction();
+//yourFunction();
+//=============================================================
+function addition(a){
+    return function(y){
+        return a+y;
+    };
+}
+var first=addition(5);
+var second=addition(10);
+console.log("5+15 = "+first(15));   // answer will be 20
+console.log("10+30 = "+second(30)); // 40
